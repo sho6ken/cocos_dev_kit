09a18ac018dddf2b1ff1b566a40a2a07122a74a0
Kent <>
2025-01-23 12:05:53 +0800

    feat: canvas適配

---
+++ custom_cmpt/adapt/canvas_adapt.ts
@@ -0,0 +1,28 @@
+import { EventModule } from "../../event/event_module";
+import { Singleton } from "../../singleton/singleton";
+
+const { ccclass, property, menu } = cc._decorator;
+
+/**
+ * canvas適配
+ * @summary https://www.jianshu.com/p/24cba3de1e33
+ * 強制使用訂寬高(show all), 方便其他適配元件計算變化
+ */
+@ccclass
+@menu("custom cmpt/canvas adapt")
+export class CanvasAdapt extends cc.Component {
+    /**
+     * 
+     */
+    protected onLoad(): void {
+        // 定寬高
+        let canvas = cc.Canvas.instance;
+        canvas.fitWidth = true;
+        canvas.fitHeight = true; 
+
+        // 視窗變化事件
+        cc.view.setResizeCallback(() => {
+            Singleton.get(EventModule).emit("ViewResize");
+        });
+    }
+}
